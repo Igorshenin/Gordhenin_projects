0cf83326f2422c1d0831af5bfd964dabfe9a1cd0
Igorshenin <92224065+Igorshenin@users.noreply.github.com>
2021-12-18 22:13:08 +0300

    Конфигурация

---
+++ Exchange_bot/config.py
@@ -0,0 +1,10 @@
+TOKEN = ''  # Ваш Токен Telegram bot
+
+TOKEN_CURRENCY = '29ca99f0e845893f2fea24fe5ed1adb9'  # Токен сайта валют exchangeratesapi.io
+
+keys = {
+    'биткоин': 'BTC',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'фунт': 'GBP'
